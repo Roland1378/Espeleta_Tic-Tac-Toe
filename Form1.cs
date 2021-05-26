3796f9b359136542dbb5915211c96a884d0ba3b9
Roland Espeleta <rolandvictor1378@gmail.com>
2021-05-26 19:36:05 +0800

    created the winner method

--- Form1.cs
+++ Form1.cs
@@ -13,9 +13,11 @@ namespace Espeleta_Tic_Tac_Toe
     public partial class tic_tac_toe : Form
     {
         public tic_tac_toe()
-        {
-            InitializeComponent();
-        }
+        
+            {
+                InitializeComponent();
+            }
+        
 
         private void tic_tac_toe_Load(object sender, EventArgs e)
         {
@@ -33,7 +35,7 @@ namespace Espeleta_Tic_Tac_Toe
         public void choose_slot(object sender, EventArgs e)
         {
             Button press = (Button)sender;
-            if(press.Text.Equals(""))
+            if (press.Text.Equals(""))
             {
 
             }
@@ -42,15 +44,100 @@ namespace Espeleta_Tic_Tac_Toe
                 press.Text = "X";
                 message.Text = "Your Turn, Player O";
                 press.ForeColor = Color.Red;
+                winner();
             }
             else
             {
                 press.Text = "O";
                 message.Text = "Your Turn, Player X";
                 press.ForeColor = Color.Green;
-
+                winner();
             }
             playing++;
+        }
+        bool win = false;
+        public void winner()
+        {
+             if (slot1.Text == "X" && slot2.Text == "X" && slot3.Text == "X")
+            {
+                win = true;
+                message.Text = "Congratulations Player X";
+            }
+            if (slot4.Text == "X" && slot5.Text == "X" && slot6.Text == "X")
+            {
+                win = true;
+                message.Text = "Congratulations Player X";
+            }
+            if (slot7.Text == "X" && slot8.Text == "X" && slot9.Text == "X")
+            {
+                win = true;
+                message.Text = "Congratulations Player X";
+            }
+            if (slot1.Text == "X" && slot4.Text == "X" && slot7.Text == "X")
+            {
+                win = true;
+                message.Text = "Congratulations Player X";
+            }
+            if (slot2.Text == "X" && slot4.Text == "X" && slot8.Text == "X")
+            {
+                win = true;
+                message.Text = "Congratulations Player X";
+            }
+            if (slot3.Text == "X" && slot6.Text == "X" && slot9.Text == "X")
+            {
+                win = true;
+                message.Text = "Congratulations Player X";
+            }
+            if (slot1.Text == "X" && slot5.Text == "X" && slot9.Text == "X")
+            {
+                win = true;
+                message.Text = "Congratulations Player X";
+            }
+            if (slot3.Text == "X" && slot5.Text == "X" && slot7.Text == "X")
+            {
+                win = true;
+                message.Text = "Congratulations Player X";
+            }
+            if (slot1.Text == "O" && slot2.Text == "O" && slot3.Text == "O")
+            {
+                win = true;
+                message.Text = "Congratulations Player X";
+            }
+            if (slot4.Text == "O" && slot5.Text == "O" && slot6.Text == "O")
+            {
+                win = true;
+                message.Text = "Congratulations Player O";
+            }
+            if (slot7.Text == "O" && slot8.Text == "O" && slot9.Text == "O")
+            {
+                win = true;
+                message.Text = "Congratulations Player O";
+            }
+            if (slot1.Text == "O" && slot4.Text == "O" && slot7.Text == "O")
+            {
+                win = true;
+                message.Text = "Congratulations Player O";
+            }
+            if (slot2.Text == "O" && slot4.Text == "O" && slot8.Text == "O")
+            {
+                win = true;
+                message.Text = "Congratulations Player O";
+            }
+            if (slot3.Text == "O" && slot6.Text == "O" && slot9.Text == "O")
+            {
+                win = true;
+                message.Text = "Congratulations Player O";
+            }
+            if (slot1.Text == "O" && slot5.Text == "O" && slot9.Text == "O")
+            {
+                win = true;
+                message.Text = "Congratulations Player O";
+            }
+            if (slot3.Text == "O" && slot5.Text == "O" && slot7.Text == "O")
+            {
+                win = true;
+                message.Text = "Congratulations Player O";
             }
         }
     }
+}
